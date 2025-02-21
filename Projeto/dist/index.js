f7464977dc7a3ccbc4fdf9cebfcc03575958c766
isabelleLimaCode <a39617@alunos.ipb.pt>
2025-02-21 21:57:32 +0000

    mini projeto concluido

--- Projeto/dist/index.js
+++ Projeto/dist/index.js
@@ -2,32 +2,34 @@
 class TodoApp {
     constructor(taskList) {
         this.tasks = [];
-        this.netxId = 0;
+        this.nextId = 0;
         this.taskList = taskList;
     }
     //metodos
     //add
     add(title) {
-        const task = { id: this.netxId++, title };
+        const task = { id: this.nextId++, title };
         this.tasks.push(task);
         this.render();
     }
     //remover
     remove(id) {
         this.tasks = this.tasks.filter(item => item.id !== id);
+        this.render();
     }
     //render
     render() {
-        this.taskList.innerHTML = '';
-        this.tasks.forEach(task => {
+        this.taskList.innerHTML = "";
+        this.tasks.forEach((task) => {
             const li = document.createElement('li');
             li.className = "flex w-full border-solid border-[1px] rounded justify-between items-center p-2 mb-1";
             const title = document.createElement('p');
             title.textContent = task.title;
             li.appendChild(title);
             const button = document.createElement('button');
-            button.className = "bg-red-500 text-white px-2 py-1 rounded";
-            button.textContent = 'Deletar';
+            button.className = "bg-red-500 text-white px-2 py-1 rounded cursor-pointer";
+            button.textContent = "Deletar";
+            button.addEventListener('click', () => this.remove(task.id));
             li.appendChild(button);
             this.taskList.appendChild(li);
         });
@@ -38,7 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const tasknew = document.getElementById('newTask');
     const taskAdd = document.getElementById('addTask');
     const todoApp = new TodoApp(taskList);
-    taskAdd.addEventListener('click', () => {
+    taskAdd.addEventListener('click', (event) => {
+        event.preventDefault();
         const title = tasknew.value.trim();
         if (title) {
             todoApp.add(title);
--- Projeto/src/index.ts
+++ Projeto/src/index.ts
@@ -1,5 +1,5 @@
 
-interface task{
+interface Task{
     id: number;
     title: string;
 }
@@ -7,8 +7,9 @@ interface task{
 
 class TodoApp {
     private taskList: HTMLUListElement;
-    private tasks:task[] = [];
-    private netxId: number = 0;
+    private tasks:Task[] = [];
+    private nextId: number = 0;
+
 
     constructor(taskList: HTMLUListElement){
         this.taskList = taskList;
@@ -17,39 +18,39 @@ class TodoApp {
     //metodos
     //add
     add(title: string): void{
-        const task: task ={ id: this.netxId++, title};
+        const task: Task ={ id: this.nextId++, title};
         this.tasks.push(task);
         this.render();
     }
     //remover
     remove(id: number): void{
         this.tasks = this.tasks.filter(item => item.id !== id);
+        this.render();
     }
 
     //render
 
-    private render(): void{
+    private render(): void {
         this.taskList.innerHTML = "";
         this.tasks.forEach((task) => {
-
             const li = document.createElement('li');
-            li.className ="flex w-full border-solid border-[1px] rounded justify-between items-center p-2 mb-1";
-
-
+            li.className = "flex w-full border-solid border-[1px] rounded justify-between items-center p-2 mb-1";
+    
             const title = document.createElement('p');
             title.textContent = task.title;
             li.appendChild(title);
-
+    
             const button = document.createElement('button');
-            button.className ="bg-red-500 text-white px-2 py-2 rounded";
-            button.textContent = 'Deletar';
-
+            button.className = "bg-red-500 text-white px-2 py-1 rounded cursor-pointer";
+            button.textContent = "Deletar";
+    
             button.addEventListener('click', () => this.remove(task.id));
-
+    
             li.appendChild(button);
             this.taskList.appendChild(li);
         });
     }
+    
 }
 
 
